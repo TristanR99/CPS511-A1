c98dc0e5e2afb5cac891a96d6c20f94a7ee3fa00
TristanR99 <tristanrebello@gmail.com>
2019-09-28 08:53:58 -0400

    Added spinning propeller and forward/backward movement

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*******************************************************************
-           Multi-Part Model Construction and Manipulation
+		   Multi-Part Model Construction and Manipulation
 ********************************************************************/
 
 #include <stdio.h>
@@ -13,8 +13,12 @@ const int meshSize = 16;    // Default Mesh Size
 const int vWidth = 650;     // Viewport width in pixels
 const int vHeight = 500;    // Viewport height in pixels
 
+static int isON = 0;
 static int currentButton;
 static unsigned char currentKey;
+static double rotate_degree = 0.0;
+static double prop_rotate_degree_update = 0.0;
+static double sub_motion = 0.0;
 
 // Lighting/shading and material properties for drone - upcoming lecture - just copy for now
 
@@ -48,204 +52,229 @@ void mouse(int button, int state, int x, int y);
 void mouseMotionHandler(int xMouse, int yMouse);
 void keyboard(unsigned char key, int x, int y);
 void functionKeys(int key, int x, int y);
+void timer(int value);
 Vector3D ScreenToWorld(int x, int y);
 
+void drawSub(void);
+void drawBody(void);
+void drawPropeller(void);
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    // Initialize GLUT
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(vWidth, vHeight);
-    glutInitWindowPosition(200, 30);
-    glutCreateWindow("Assignment 1");
+	// Initialize GLUT
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(vWidth, vHeight);
+	glutInitWindowPosition(200, 30);
+	glutCreateWindow("Assignment 1");
 
-    // Initialize GL
-    initOpenGL(vWidth, vHeight);
+	// Initialize GL
+	initOpenGL(vWidth, vHeight);
 
-    // Register callbacks
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutMouseFunc(mouse);
-    glutMotionFunc(mouseMotionHandler);
-    glutKeyboardFunc(keyboard);
-    glutSpecialFunc(functionKeys);
+	// Register callbacks
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+	glutMotionFunc(mouseMotionHandler);
+	glutKeyboardFunc(keyboard);
+	glutSpecialFunc(functionKeys);
+	glutTimerFunc(0, *timer, 0);
 
-    // Start event loop, never returns
-    glutMainLoop();
 
-    return 0;
+	// Start event loop, never returns
+	glutMainLoop();
+
+	return 0;
 }
 
 
 // Set up OpenGL. For viewport and projection setup see reshape(). */
 void initOpenGL(int w, int h)
 {
-    // Set up and enable lighting
-    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient);
-    glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular);
+	// Set up and enable lighting
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, light_specular);
 
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position0);
-    glLightfv(GL_LIGHT1, GL_POSITION, light_position1);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    //glEnable(GL_LIGHT1);   // This light is currently off
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position0);
+	glLightfv(GL_LIGHT1, GL_POSITION, light_position1);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	//glEnable(GL_LIGHT1);   // This light is currently off
 
-    // Other OpenGL setup
-    glEnable(GL_DEPTH_TEST);   // Remove hidded surfaces
-    glShadeModel(GL_SMOOTH);   // Use smooth shading, makes boundaries between polygons harder to see 
-    glClearColor(0.6F, 0.6F, 0.6F, 0.0F);  // Color and depth for glClear
-    glClearDepth(1.0f);
-    glEnable(GL_NORMALIZE);    // Renormalize normal vectors 
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);   // Nicer perspective
+	// Other OpenGL setup
+	glEnable(GL_DEPTH_TEST);   // Remove hidded surfaces
+	glShadeModel(GL_SMOOTH);   // Use smooth shading, makes boundaries between polygons harder to see 
+	glClearColor(0.6F, 0.6F, 0.6F, 0.0F);  // Color and depth for glClear
+	glClearDepth(1.0f);
+	glEnable(GL_NORMALIZE);    // Renormalize normal vectors 
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);   // Nicer perspective
 
-    // Set up ground quad mesh
-    Vector3D origin = NewVector3D(-8.0f, 0.0f, 8.0f);
-    Vector3D dir1v = NewVector3D(1.0f, 0.0f, 0.0f);
-    Vector3D dir2v = NewVector3D(0.0f, 0.0f, -1.0f);
-    groundMesh = NewQuadMesh(meshSize);
-    InitMeshQM(&groundMesh, meshSize, origin, 16.0, 16.0, dir1v, dir2v);
+	// Set up ground quad mesh
+	Vector3D origin = NewVector3D(-8.0f, 0.0f, 8.0f);
+	Vector3D dir1v = NewVector3D(1.0f, 0.0f, 0.0f);
+	Vector3D dir2v = NewVector3D(0.0f, 0.0f, -1.0f);
+	groundMesh = NewQuadMesh(meshSize);
+	InitMeshQM(&groundMesh, meshSize, origin, 16.0, 16.0, dir1v, dir2v);
 
-    Vector3D ambient = NewVector3D(0.0f, 0.05f, 0.0f);
-    Vector3D diffuse = NewVector3D(0.4f, 0.8f, 0.4f);
-    Vector3D specular = NewVector3D(0.04f, 0.04f, 0.04f);
-    SetMaterialQM(&groundMesh, ambient, diffuse, specular, 0.2);
+	Vector3D ambient = NewVector3D(0.0f, 0.05f, 0.0f);
+	Vector3D diffuse = NewVector3D(0.4f, 0.8f, 0.4f);
+	Vector3D specular = NewVector3D(0.04f, 0.04f, 0.04f);
+	SetMaterialQM(&groundMesh, ambient, diffuse, specular, 0.2);
 
-    // Set up the bounding box of the scene
-    // Currently unused. You could set up bounding boxes for your objects eventually.
-    //Set(&BBox.min, -8.0f, 0.0, -8.0);
-    //Set(&BBox.max, 8.0f, 6.0,  8.0);
+	// Set up the bounding box of the scene
+	// Currently unused. You could set up bounding boxes for your objects eventually.
+	//Set(&BBox.min, -8.0f, 0.0, -8.0);
+	//Set(&BBox.max, 8.0f, 6.0,  8.0);
 }
 
+void timer(int value) {
+	if (isON)
+		prop_rotate_degree_update += 10;
+	glutTimerFunc(16, *timer, 0);
+	glutPostRedisplay();
+}
 
 // Callback, called whenever GLUT determines that the window should be redisplayed
 // or glutPostRedisplay() has been called.
 void display(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Draw Drone
+	// Draw Drone
 
-    // Set drone material properties
-    glMaterialfv(GL_FRONT, GL_AMBIENT, drone_mat_ambient);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, drone_mat_specular);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, drone_mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SHININESS, drone_mat_shininess);
+	// Set drone material properties
+	glMaterialfv(GL_FRONT, GL_AMBIENT, drone_mat_ambient);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, drone_mat_specular);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, drone_mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SHININESS, drone_mat_shininess);
 
-    // Apply transformations to move drone
-    // ...
+	drawSub();
 
-    // Apply transformations to construct drone, modify this!
-    glPushMatrix();
-    glTranslatef(0.0, 4.0, 0.0);
-    glRotatef(45.0, 0.0, 1.0, 0.0);
-    glScalef(6.0, 1.0, 1.0);
-    glutSolidCube(1.0);
-    glPopMatrix();
+	// Draw ground mesh
+	DrawMeshQM(&groundMesh, meshSize);
+	glutSwapBuffers();   // Double buffering, swap buffers
+}
 
-    // Draw ground mesh
-    DrawMeshQM(&groundMesh, meshSize);
+void drawSub(void) {
+	drawBody();
+	drawPropeller();
+}
 
-    glutSwapBuffers();   // Double buffering, swap buffers
+void drawBody(void) {
+	// Creates body of sub
+	glPushMatrix();
+	glTranslatef(sub_motion, 4.0, 0.0);
+	glRotatef(rotate_degree, 0.0, 1.0, 0.0);
+	glScalef(6.0, 1.0, 1.0);
+	glutSolidSphere(1.0, 360, 360);
+	glPopMatrix();
+}
+
+void drawPropeller(void) {
+
+	// Creates propeller of sub
+	glPushMatrix();
+	glTranslatef(6.0 + sub_motion, 4.0, 0.0);
+	glRotatef(90, 0.0, 1.0, 0.0);
+	glRotatef(prop_rotate_degree_update, 0.0, 0.0, 1.0);
+	glScalef(0.1, 1.0, 0.5);
+	glutSolidCube(2.0);
+	glPopMatrix();
 }
 
 
 // Callback, called at initialization and whenever user resizes the window.
 void reshape(int w, int h)
 {
-    // Set up viewport, projection, then change to modelview matrix mode - 
-    // display function will then set up camera and do modeling transforms.
-    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	// Set up viewport, projection, then change to modelview matrix mode - 
+	// display function will then set up camera and do modeling transforms.
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60.0, (GLdouble)w / h, 0.2, 40.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, (GLdouble)w / h, 0.2, 40.0);
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 
-    // Set up the camera at position (0, 6, 22) looking at the origin, up along positive y axis
-    gluLookAt(0.0, 6.0, 22.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	// Set up the camera at position (0, 6, 22) looking at the origin, up along positive y axis
+	gluLookAt(0.0, 15.0, 22.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
 // Callback, handles input from the keyboard, non-arrow keys
 void keyboard(unsigned char key, int x, int y)
 {
-    switch (key)
-    {
-    case 't':
-
-        break;
-    }
-
-    glutPostRedisplay();   // Trigger a window redisplay
+	if (key == 's')
+		isON = !isON;
+	else if (key == 'f')
+		sub_motion -= 0.5;
+	else if (key == 'b')
+		sub_motion += 0.5;
+	
+	glutPostRedisplay();   // Trigger a window redisplay
 }
 
 // Callback, handles input from the keyboard, function and arrow keys
 void functionKeys(int key, int x, int y)
 {
-    // Help key
-    if (key == GLUT_KEY_F1)
-    {
 
-    }
-    // Do transformations with arrow keys
-    //else if (...)   // GLUT_KEY_DOWN, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_LEFT
-    //{
-    //}
+	if(key == GLUT_KEY_LEFT)
+		rotate_degree += 10;
+	else if(key == GLUT_KEY_RIGHT)
+		rotate_degree -= 10;
 
-    glutPostRedisplay();   // Trigger a window redisplay
+	glutPostRedisplay();   // Trigger a window redisplay
 }
 
 
 // Mouse button callback - use only if you want to 
 void mouse(int button, int state, int x, int y)
 {
-    currentButton = button;
+	currentButton = button;
 
-    switch (button)
-    {
-    case GLUT_LEFT_BUTTON:
-        if (state == GLUT_DOWN)
-        {
-            ;
+	switch (button)
+	{
+	case GLUT_LEFT_BUTTON:
+		if (state == GLUT_DOWN)
+		{
+			;
 
-        }
-        break;
-    case GLUT_RIGHT_BUTTON:
-        if (state == GLUT_DOWN)
-        {
-            ;
-        }
-        break;
-    default:
-        break;
-    }
+		}
+		break;
+	case GLUT_RIGHT_BUTTON:
+		if (state == GLUT_DOWN)
+		{
+			;
+		}
+		break;
+	default:
+		break;
+	}
 
-    glutPostRedisplay();   // Trigger a window redisplay
+	glutPostRedisplay();   // Trigger a window redisplay
 }
 
 
 // Mouse motion callback - use only if you want to 
 void mouseMotionHandler(int xMouse, int yMouse)
 {
-    if (currentButton == GLUT_LEFT_BUTTON)
-    {
-        ;
-    }
+	if (currentButton == GLUT_LEFT_BUTTON)
+	{
+		;
+	}
 
-    glutPostRedisplay();   // Trigger a window redisplay
+	glutPostRedisplay();   // Trigger a window redisplay
 }
 
 
 Vector3D ScreenToWorld(int x, int y)
 {
-    // you will need to finish this if you use the mouse
-    return NewVector3D(0, 0, 0);
+	// you will need to finish this if you use the mouse
+	return NewVector3D(0, 0, 0);
 }
 
 
